52ad1a021f5c30c16730dbb654be5787b2a9b8fb
Laurens <laurenskz@gmail.com>
2021-10-30 09:35:28 +0200

    Update conf

--- src/main/java/A/B/NethackConfiguration.java
+++ src/main/java/A/B/NethackConfiguration.java
@@ -4,9 +4,9 @@ package A.B;
  * This class specifies the initial configuration for Nethack that we want to launch.
  */
 public class NethackConfiguration {
-    public int seed = 2;
-    public int rows = 40;
-    public int columns = 40;
+    public int seed = 16;
+    public int rows = 25;
+    public int columns = 25;
     public int roomCount = 7;
 
     public int minMobs = 0;
